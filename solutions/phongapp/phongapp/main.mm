f555e63597af5675a31b2fd8ee6db5173ecce403
Junch <wolf76@wolf76tekiiMac.local>
2013-02-14 15:29:27 +0800

    Add a xcode app which contains the two shaders

---
+++ solutions/phongapp/phongapp/main.mm
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  phongapp
+//
+//  Created by wolf76 on 13-2-14.
+//  Copyright (c) 2013年 wolf76. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
